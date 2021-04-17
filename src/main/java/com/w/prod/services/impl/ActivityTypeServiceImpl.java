baed6231c4d56ba739bd792f806ca95944fa72da
Miroslav Mirchev <mirchev1977@yahoo.com>
2021-04-17 20:53:33 +0300

    Edited seeding Activity types

--- src/main/java/com/w/prod/services/impl/ActivityTypeServiceImpl.java
+++ src/main/java/com/w/prod/services/impl/ActivityTypeServiceImpl.java
@@ -5,6 +5,7 @@ import com.w.prod.models.service.ActivityTypeServiceModel;
 import com.w.prod.repositories.ActivityTypeRepository;
 import com.w.prod.services.ActivityTypeService;
 import com.google.gson.Gson;
+import org.hibernate.property.access.internal.PropertyAccessStrategyIndexBackRefImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -13,24 +14,22 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.Array;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class ActivityTypeServiceImpl implements ActivityTypeService {
-    private final Resource activityTypeFile;
+//    private final Resource activityTypeFile;
     private final Gson gson;
     private final ActivityTypeRepository activityTypeRepository;
     private final ModelMapper modelMapper;
 
     public ActivityTypeServiceImpl(
-            @Value("classpath:init/activityType.json") Resource activityTypeFile,
             Gson gson,
             ActivityTypeRepository activityTypeRepository,
             ModelMapper modelMapper
     ) {
-        this.activityTypeFile = activityTypeFile;
         this.gson = gson;
         this.activityTypeRepository = activityTypeRepository;
         this.modelMapper = modelMapper;
@@ -40,18 +39,14 @@ public class ActivityTypeServiceImpl implements ActivityTypeService {
     @Override
     public void seedActivityTypes() {
         if (activityTypeRepository.count() == 0) {
-            try {
-                ActivityTypeServiceModel[] activityTypeServiceModels = gson.fromJson(Files.readString(Path.of(activityTypeFile.getURI())), ActivityTypeServiceModel[].class);
-                Arrays.stream(activityTypeServiceModels)
-                        .forEach(m -> {
-                            ActivityType current = modelMapper.map(m, ActivityType.class);
-                            activityTypeRepository.save(current);
-                        });
+            ArrayList<String> activityTypes = new ArrayList<>();
+            activityTypes.add("Business Support");
 
-            } catch (IOException e) {
-                throw new IllegalStateException("Cannot seed Activity Types");
+            for (String activity : activityTypes) {
+                ActivityType act = new ActivityType();
+                act.setActivityName(activity);
+                activityTypeRepository.save(act);
             }
-
         }
     }
 
